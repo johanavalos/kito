c5e182611812be23fbee7f58d301862de9809e33
johanavalos <johan.avalos.02@gmail.com>
2024-12-29 23:16:31 -0600

    Additional validation for new employee added

--- hiber-api/src/main/java/com/example/hiber_api/dto/NewEmployeeDTO.java
+++ hiber-api/src/main/java/com/example/hiber_api/dto/NewEmployeeDTO.java
@@ -2,20 +2,30 @@ package com.example.hiber_api.dto;
 
 import java.util.Date;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 
 public class NewEmployeeDTO {
     
     @NotNull
+    @NotBlank
     private String name;
 
     @NotNull
+    @NotBlank
+    @Pattern(regexp = "(fe)?male", message = "must be 'male' or 'female'")
     private String gender;
 
     @NotNull
+    @NotBlank
     private String department;
 
     @NotNull
+    @NotBlank
+    @JsonFormat(pattern = "MM/dd/yyyy")
     private Date dob;
 
     public String getName() {
