a96ec351f06caae1fa763d5ccb38817016957dba
johanavalos <johan.avalos.02@gmail.com>
2025-01-14 20:48:43 -0600

    Classes for security

---
+++ hiber-api/src/main/java/com/example/hiber_api/model/security/Permission.java
@@ -0,0 +1,21 @@
+package com.example.hiber_api.model.security;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Setter
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "permissions")
+public class Permission {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false, updatable = false)
+    private String name;
+}
---
+++ hiber-api/src/main/java/com/example/hiber_api/model/security/RoleEnum.java
@@ -0,0 +1,8 @@
+package com.example.hiber_api.model.security;
+
+public enum RoleEnum {
+    ADMIN,
+    USER,
+    INVITED,
+    DEVELOPER
+}
