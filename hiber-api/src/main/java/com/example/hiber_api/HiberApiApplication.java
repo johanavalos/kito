010a496ac071ad70fdedb5de88c80aa4221c732b
johanavalos <johan.avalos.02@gmail.com>
2025-01-14 20:43:41 -0600

    Procedure call added in init

--- hiber-api/src/main/java/com/example/hiber_api/HiberApiApplication.java
+++ hiber-api/src/main/java/com/example/hiber_api/HiberApiApplication.java
@@ -1,7 +1,11 @@
 package com.example.hiber_api;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.example.hiber_api.repository.EmployeeRepository;
 
 @SpringBootApplication
 public class HiberApiApplication {
@@ -9,5 +13,12 @@ public class HiberApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HiberApiApplication.class, args);
 	}
+
+    @Bean
+    CommandLineRunner init(EmployeeRepository employeeRepository) {
+        return args -> {
+            employeeRepository.createEmployees();
+        };
+    }
 
 }
--- hiber-api/src/main/java/com/example/hiber_api/repository/EmployeeRepository.java
+++ hiber-api/src/main/java/com/example/hiber_api/repository/EmployeeRepository.java
@@ -3,6 +3,7 @@ package com.example.hiber_api.repository;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.query.Procedure;
 import org.springframework.data.repository.CrudRepository;
 
 import com.example.hiber_api.dto.EmployeeDTO;
@@ -13,4 +14,7 @@ public interface EmployeeRepository extends CrudRepository<Employee, Integer> {
     List<EmployeeDTO> findBy();
 
     Optional<EmployeeDTO> findEmployeeDTOById(Integer id);
+
+    @Procedure(procedureName = "InsertEmployeesAndTasks")
+    void createEmployees();
 }
