9a18812e208c0680bd8165a032aeb420013548e6
johanavalos <johan.avalos.02@gmail.com>
2025-01-21 12:52:46 -0600

    User creation service method implemented

--- hiber-api/src/main/java/com/example/hiber_api/dto/auth/AuthCreateUserRequest.java
+++ hiber-api/src/main/java/com/example/hiber_api/dto/auth/AuthCreateUserRequest.java
@@ -1,9 +1,11 @@
 package com.example.hiber_api.dto.auth;
 
-import jakarta.validation.Valid;
+import java.util.List;
+
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public record AuthCreateUserRequest(@NotBlank String username,
                                     @NotBlank String password,
-                                    @Valid AuthCreateRoleRequest roleRequest) {
+                                    @Size(min = 1, max=3) List<String> roles) {
 }
--- hiber-api/src/main/java/com/example/hiber_api/service/UserDetailsServiceImpl.java
+++ hiber-api/src/main/java/com/example/hiber_api/service/UserDetailsServiceImpl.java
@@ -4,8 +4,11 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -19,7 +22,11 @@ import org.springframework.stereotype.Service;
 import com.example.hiber_api.dto.auth.AuthCreateUserRequest;
 import com.example.hiber_api.dto.auth.AuthLoginRequest;
 import com.example.hiber_api.dto.auth.AuthResponse;
+import com.example.hiber_api.exception.RoleDoesNotExistException;
+import com.example.hiber_api.exception.UsernameAlreadyExistsException;
+import com.example.hiber_api.model.security.Role;
 import com.example.hiber_api.model.security.User;
+import com.example.hiber_api.repository.RoleRepository;
 import com.example.hiber_api.repository.UserRepository;
 import com.example.hiber_api.util.JwtUtils;
 
@@ -27,7 +34,10 @@ import com.example.hiber_api.util.JwtUtils;
 public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Autowired
-    private UserRepository repository;
+    private UserRepository userRepository;
+
+    @Autowired
+    private RoleRepository roleRepository;
 
     @Autowired
     private JwtUtils jwtUtils;
@@ -38,7 +48,7 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
-        User user = repository.findByUsername(username)
+        User user = userRepository.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User " + username + " does not exist."));
 
         List<SimpleGrantedAuthority> authorities = new ArrayList<>();
@@ -88,8 +98,57 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     }
 
     public AuthResponse createUser(AuthCreateUserRequest userRequest) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'createUser'");
+        String username = userRequest.username();
+        String password = userRequest.password();
+        Set<Role> roles = extractRoles(userRequest);
+
+        User userEntity = User.builder()
+            .username(username)
+            .password(passwordEncoder.encode(password))
+            .roles(roles).isEnabled(true)
+            .accountNoLocked(true)
+            .accountNoExpired(true)
+            .credentialNoExpired(true)
+            .build();
+
+        User userSaved;
+        try {
+            userSaved = userRepository.save(userEntity);
+        } catch (DataIntegrityViolationException e) {
+            throw new UsernameAlreadyExistsException(username);
+        }
+
+        ArrayList<SimpleGrantedAuthority> authorities = new ArrayList<>();
+
+        userSaved.getRoles().forEach(role -> 
+            authorities.add(
+                new SimpleGrantedAuthority("ROLE_".concat(role.getRoleEnum().name()))));
+
+        userSaved.getRoles().stream()
+            .flatMap(role -> role.getPermissions().stream())
+            .forEach(permission -> authorities.add(new SimpleGrantedAuthority(permission.getName())));
+
+        Authentication authentication = new UsernamePasswordAuthenticationToken(userSaved, null, authorities);
+
+        String accessToken = jwtUtils.createToken(authentication);
+
+        AuthResponse authResponse = new AuthResponse(username, "User created successfully", accessToken, true);
+        return authResponse;
     }
 
+    private Set<Role> extractRoles(AuthCreateUserRequest userRequest) {
+        List<String> rolesRequest = userRequest.roles();
+
+        Set<Role> roles;
+        try {
+            roles = roleRepository.findRoleEntitiesByRoleEnumIn(rolesRequest).stream().collect(Collectors.toSet());
+        } catch (Exception e) {
+            System.err.println(e.getClass());
+            System.err.println(e.getMessage());
+            String[] splitted = e.getMessage().split("\\.");
+            String role = splitted[splitted.length - 1];
+            throw new RoleDoesNotExistException(role);
+        }
+        return roles;
+    }
 }
